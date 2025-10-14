26f75e9594d0e4366112075484984464a5b579cd
Jaro Habiger <jarohabiger@googlemail.com>
2025-10-14 21:54:50 +0200

    download model if not present in tests

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -1,6 +1,6 @@
 import os
 from decent_whisper.backends import mlx_whisper, faster_whisper
-from decent_whisper.model import choose_model
+from decent_whisper.model import choose_model, download_model, is_model_downloaded
 
 def run_test_for_backend(backend):
     model_info = choose_model(
@@ -9,12 +9,17 @@ def run_test_for_backend(backend):
     )
     assert model_info is not None
 
+    if not is_model_downloaded(model_info):
+        download_model(model_info, lambda loaded, total: None)
+
+
     iter, info = backend.transcribe(os.path.dirname(os.path.realpath(__file__)) + "/data/audio.mp3", model=model_info)
 
     for segment in iter:
         assert segment is not None
 
     assert info.language == "en" # how flaky is this?
+    print(f"sucessfully tested {backend}")
 
 
 def test_mlx_whisper():
